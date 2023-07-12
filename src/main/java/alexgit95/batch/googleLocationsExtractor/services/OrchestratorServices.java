189de330fa1fff965fe0e26706ac63291417e5b9
Alex B <>
2023-07-12 15:09:36 +0200

    refacto + update readme

--- src/main/java/alexgit95/batch/googleLocationsExtractor/services/OrchestratorServices.java
+++ src/main/java/alexgit95/batch/googleLocationsExtractor/services/OrchestratorServices.java
@@ -5,7 +5,9 @@ import alexgit95.batch.googleLocationsExtractor.dao.entities.LocationsOutput;
 import java.io.File;
 import java.util.List;
 
-public interface FileServices {
+public interface OrchestratorServices {
+
+    void processGoogleFiles(List<LocationsOutput> result);
     void processFile(File src, List<LocationsOutput> result);
 
     void saveAll(List<LocationsOutput> result);
